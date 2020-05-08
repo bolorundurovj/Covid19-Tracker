e3532dbb4cc484dc186086a32e01d57f727aa2f8
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-08 15:52:39 +0100

    Update data-service.service.ts

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataServiceService {
+  private globalDataUrl = `https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/05-07-2020.csv`;
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getGlobalData() {
+    return this.http.get(this.globalDataUrl).pipe(
+      map((result) => {
+        console.log(result);
+      })
+    );
+  }
 }
