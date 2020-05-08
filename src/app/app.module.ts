e61b7a72082d0b30aa7cc928fb51f24e0956039a
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-08 15:06:57 +0100

    Generated countries component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
+import { CountriesComponent } from './components/countries/countries.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    CountriesComponent
   ],
   imports: [
     BrowserModule,
