5d2a36f9a96003f8d5945525f7186d4978cfaf59
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-09 16:14:04 +0100

    Update countries.component.ts

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -5,7 +5,7 @@ import { GlobalDataSummary } from '../../models/global-data';
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
-  styleUrls: ['./countries.component.css']
+  styleUrls: ['./countries.component.css'],
 })
 export class CountriesComponent implements OnInit {
   totalConfirmed = 0;
@@ -15,20 +15,25 @@ export class CountriesComponent implements OnInit {
 
   data: GlobalDataSummary[];
   countries: string[] = [];
-  constructor(private service : DataServiceService) { }
+  constructor(private service: DataServiceService) {}
 
   ngOnInit(): void {
-    this.service.getGlobalData().subscribe(result => {
+    this.service.getGlobalData().subscribe((result) => {
       this.data = result;
-      this.data.forEach(cs => {
+      this.data.forEach((cs) => {
         this.countries.push(cs.country);
-      })
-
+      });
     });
   }
 
-  updateValues(country : string) {
-
+  updateValues(country: string) {
+    this.data.forEach((cs) => {
+      if (cs.country == country) {
+        this.totalActive += cs.active;
+        this.totalConfirmed += cs.confirmed;
+        this.totalDeaths += cs.deaths;
+        this.totalRecovered += cs.recovered;
+      }
+    });
   }
-
 }
