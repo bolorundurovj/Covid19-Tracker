b051dfa1a4e16d1b74bd081beea2ae2cb11032e6
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-09 10:45:36 +0100

    Update countries.component.ts

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from 'src/app/services/data-service.service';
 
 @Component({
   selector: 'app-countries',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountriesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service : DataServiceService) { }
 
   ngOnInit(): void {
   }
