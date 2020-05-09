5c9c50a6462acf4fe94d8803b43e5cb67ceff45e
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-09 17:30:44 +0100

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -25,7 +25,7 @@ export class HomeComponent implements OnInit {
     let datatable = [];
     datatable.push(["Country", "Cases"])
     this.globalData.forEach(cs => {
-      datatable.push[cs.country, cs.confirmed]
+      datatable.push([cs.country, cs.confirmed]);
     })
     this.pieChart = {
       chartType: 'PieChart',
