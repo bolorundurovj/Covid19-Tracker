14594f55c9f21d9eaeb0e6787d0a5af91f01f943
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-09 15:09:39 +0100

    Create dashboard-cards.component.ts

---
+++ src/app/components/dashboard-cards/dashboard-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-cards',
+  templateUrl: './dashboard-cards.component.html',
+  styleUrls: ['./dashboard-cards.component.css']
+})
+export class DashboardCardsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
