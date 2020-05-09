f6a1d7a84fec6e1b2381b8adb90dd806d74b1ecf
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-09 19:38:31 +0100

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -15,58 +15,53 @@ export class HomeComponent implements OnInit {
   totalRecovered = 0;
   pieChart: GoogleChartInterface = {
     chartType: 'PieChart',
-
   };
   columnChart: GoogleChartInterface = {
     chartType: 'ColumnChart',
-
   };
-  globalData : GlobalDataSummary[] ;
+  globalData: GlobalDataSummary[];
 
-  constructor(private dataService: DataServiceService) {};
+  constructor(private dataService: DataServiceService) {}
 
-  initChart(caseType: string){
+  updateChart(input: HTMLInputElement) {
+    console.log(input.value);
+    this.initChart(input.value);
+  }
+
+  initChart(caseType: string) {
     let datatable = [];
-    datatable.push(["Country", "Cases"])
-    this.globalData.forEach(cs => {
-      let value : number;
-      if(caseType == 'c')
-        if(cs.confirmed > 2000)
-          value = cs.confirmed
+    datatable.push(['Country', 'Cases']);
+    this.globalData.forEach((cs) => {
+      let value: number;
+      if (caseType == 'c') if (cs.confirmed > 2000) value = cs.confirmed;
 
-      if(caseType == 'r')
-        if(cs.recovered > 3000)
-          value = cs.recovered
+      if (caseType == 'r') if (cs.recovered > 2000) value = cs.recovered;
 
-      if(caseType == 'a')
-        if(cs.active > 2000)
-          value = cs.active
+      if (caseType == 'a') if (cs.active > 2000) value = cs.active;
 
-      if(caseType == 'd')
-        if(cs.deaths > 1000)
-          value = cs.deaths
+      if (caseType == 'd') if (cs.deaths > 1000) value = cs.deaths;
 
       console.log(value);
 
-      datatable.push([cs.country, value])
-    })
+      datatable.push([cs.country, value]);
+    });
     this.pieChart = {
       chartType: 'PieChart',
       dataTable: datatable,
       //firstRowIsData: true,
-      options: {height: 500},
+      options: { height: 500 },
     };
     this.columnChart = {
       chartType: 'ColumnChart',
       dataTable: datatable,
       //firstRowIsData: true,
-      options: {height: 500},
+      options: { height: 500 },
     };
   }
 
   ngOnInit(): void {
     this.dataService.getGlobalData().subscribe({
-      next: ( result) => {
+      next: (result) => {
         console.log(result);
         this.globalData = result;
         result.forEach((cs) => {
@@ -76,14 +71,9 @@ export class HomeComponent implements OnInit {
             this.totalDeaths += cs.deaths;
             this.totalRecovered += cs.recovered;
           }
-        })
+        });
         this.initChart('c');
-      }
-    })
-  }
-  updateChart(input: HTMLInputElement){
-    console.log(input.value);
-    this.initChart(input.value);
-
+      },
+    });
   }
 }
