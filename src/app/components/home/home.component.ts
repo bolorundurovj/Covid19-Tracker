96b443e5da1291b6d07b63810f2e574ffdb32f93
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-09 07:56:02 +0100

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,21 +4,29 @@ import { DataServiceService } from '../../services/data-service.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  totalConfirmed =0;
+  totalActive =0;
+  totalDeaths =0;
+  totalRecovered =0;
 
-  constructor(private dataService : DataServiceService) { }
+  constructor(private dataService: DataServiceService) {}
 
   ngOnInit(): void {
-    this.dataService.getGlobalData().subscribe(
-      {
-        next: (result)=>{
-          console.log(result);
+    this.dataService.getGlobalData().subscribe({
+      next: (result) => {
+        console.log(result);
 
-        }
+        result.forEach(cs => {
+          if(!Number.isNaN(cs.confirmed))
+          this.totalActive += cs.active;
+          this.totalConfirmed += cs.confirmed;
+          this.totalDeaths += cs.deaths;
+          this.totalRecovered += cs.recovered;
+        })
       }
-    )
+    })
   }
-
 }
