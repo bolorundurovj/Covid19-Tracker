862a8da432221084396b2c89597a8d460724581d
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-09 14:37:59 +0100

    Update countries.component.ts

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataServiceService } from 'src/app/services/data-service.service';
+import { GlobalDataSummary } from 'src/app/models/global-data';
 
 @Component({
   selector: 'app-countries',
@@ -8,10 +9,13 @@ import { DataServiceService } from 'src/app/services/data-service.service';
 })
 export class CountriesComponent implements OnInit {
 
+  data: GlobalDataSummary[];
   constructor(private service : DataServiceService) { }
 
   ngOnInit(): void {
-    this.service.getGlobalData();
+    this.service.getGlobalData().subscribe(result => {
+
+    });
   }
 
 }
