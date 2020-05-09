e4f5ed6f619b7faaad760f37922024d2135e0a2a
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-09 07:33:55 +0100

    Update data-service.service.ts

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -11,36 +11,32 @@ export class DataServiceService {
   constructor(private http: HttpClient) {}
 
   getGlobalData() {
-    return this.http
-      .get(this.globalDataUrl, { responseType: 'text' })
-      .pipe(map((result) => {
-        let data : GlobalDataSummary[] = [];
+    return this.http.get(this.globalDataUrl, { responseType: 'text' }).pipe(
+      map((result) => {
+        let data: GlobalDataSummary[] = [];
         let raw = {};
         let rows = result.split('\n');
-        rows.splice(0,1);
-        rows.forEach(rows => {
+        rows.splice(0, 1);
+        rows.forEach((rows) => {
           let cols = rows.split(/,(?=\S)/);
           let cs = {
-            country : cols[3],
-            confirmed : +cols[7],
-            deaths : +cols[8],
-            recovered : +cols[9],
-            active : +cols[10],
+            country: cols[3],
+            confirmed: +cols[7],
+            deaths: +cols[8],
+            recovered: +cols[9],
+            active: +cols[10],
           };
           let temp: GlobalDataSummary = raw[cs.country];
-          if(temp){
+          if (temp) {
             temp.active = cs.active + temp.active;
             temp.confirmed = cs.confirmed + temp.confirmed;
             temp.deaths = cs.deaths + temp.deaths;
             temp.recovered = cs.recovered + temp.recovered;
-
           }
-          raw[cs.country] = cs;
-          console.log(data);
-
-        })
-
-        return [];
-      }));
+          raw[cs.country] = temp;
+        });
+        console.log(data);
+      })
+    );
   }
 }
