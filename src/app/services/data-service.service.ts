aa988a8573c9d7e09b631ddec4f26aeff65f9427
bolorundurovj <valiantjoshuabolorunduro@gmail.com>
2020-05-09 07:28:41 +0100

    Update data-service.service.ts

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -27,6 +27,7 @@ export class DataServiceService {
             recovered : +cols[9],
             active : +cols[10],
           };
+          let temp = raw[cs.country];
           raw[cs.country] = cs;
           console.log(data);
 
